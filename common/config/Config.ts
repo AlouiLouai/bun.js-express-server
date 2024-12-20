bc76bee77316affa39313620416b34ac252d3310
alouilouai <louaialoui1993@gmail.com>
2024-12-20 16:05:35 +0100

    migrate to zod for better envs management

--- common/config/Config.ts
+++ common/config/Config.ts
@@ -1,10 +1,11 @@
+import { z } from "zod";
 import Logger from "../Logger";
 
 export default class Config {
   private static instance: Config;
   private readonly logger = Logger.getInstance();
 
-  // Type definition for the environment variables
+  // Type definition for the environment variables using Zod schema
   readonly app_port: number;
   readonly db_host: string;
   readonly db_port: number;
@@ -16,13 +17,37 @@ export default class Config {
   private constructor() {
     try {
       this.logger.info("Initializing configuration...");
-      this.app_port = this.parseNumber("PORT");
-      this.db_host = this.getEnvVar("DB_HOST");
-      this.db_port = this.parseNumber("DB_PORT");
-      this.db_user = this.getEnvVar("DB_USER");
-      this.db_password = this.getEnvVar("DB_PASSWORD");
-      this.db_name = this.getEnvVar("DB_NAME");
-      this.db_url = this.getEnvVar("DATABASE_URL");
+      const envSchema = z.object({
+        PORT: z
+          .string()
+          .transform((val) => parseInt(val, 10))
+          .refine((val) => !isNaN(val), {
+            message: "PORT must be a valid number",
+          }),
+        DB_HOST: z.string(),
+        DB_PORT: z
+          .string()
+          .transform((val) => parseInt(val, 10))
+          .refine((val) => !isNaN(val), {
+            message: "DB_PORT must be a valid number",
+          }),
+        DB_USER: z.string(),
+        DB_PASSWORD: z.string(),
+        DB_NAME: z.string(),
+        DATABASE_URL: z.string(),
+      });
+
+      // Parse and validate the environment variables using Zod schema
+      const parsedEnv = envSchema.parse(Bun.env);
+
+      this.app_port = parsedEnv.PORT;
+      this.db_host = parsedEnv.DB_HOST;
+      this.db_port = parsedEnv.DB_PORT;
+      this.db_user = parsedEnv.DB_USER;
+      this.db_password = parsedEnv.DB_PASSWORD;
+      this.db_name = parsedEnv.DB_NAME;
+      this.db_url = parsedEnv.DATABASE_URL;
+
       this.logger.info("Configuration initialized successfully.");
     } catch (error: any) {
       this.logger.error(`Error initializing configuration: ${error.message}`);
@@ -36,27 +61,5 @@ export default class Config {
       Config.instance = new Config();
     }
     return Config.instance;
-  }
-
-  // handle the type || undefined from environments
-  private getEnvVar(key: string): string {
-    const value = Bun.env[key];
-    if (value === undefined) {
-      const errorMessage = `Environment variable ${key} is not defined`;
-      this.logger.error(errorMessage);
-      throw new Error(errorMessage);
-    }
-    return value;
-  }
-
-  private parseNumber(key: string): number {
-    const value = this.getEnvVar(key);
-    const parsed = parseInt(value, 10);
-    if (isNaN(parsed)) {
-      const errorMessage = `Environment variable ${key} is not a valid number`;
-      this.logger.error(errorMessage);
-      throw new Error(errorMessage);
-    }
-    return parsed;
   }
 }
--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -2,4 +2,9 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+// For production purpose enable logs for better monotoring
+// const prisma = new PrismaClient({
+//     log: ['query', 'info', 'warn', 'error'],
+//   });
+
 export default prisma;
