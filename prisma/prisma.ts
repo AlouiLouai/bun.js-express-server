6064f7d0b5ac09a1ce79406b330aeb6ea0e544ca
alouilouai <louaialoui1993@gmail.com>
2024-12-20 16:57:53 +0100

    adding middleware to prisma queries

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,10 +1,15 @@
 import { PrismaClient } from "@prisma/client";
+import PrismaMiddleware from "./prisma.middleware";
 
+// Create an instance of PrismaClient
 const prisma = new PrismaClient();
 
 // For production purpose enable logs for better monotoring
 // const prisma = new PrismaClient({
 //     log: ['query', 'info', 'warn', 'error'],
 //   });
+
+// Apply middleware to the Prisma Client
+new PrismaMiddleware(prisma);
 
 export default prisma;
