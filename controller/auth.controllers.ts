f9c97d4dc5380a6014332b9e293a4b081b3d3ea1
alouilouai <louaialoui1993@gmail.com>
2024-12-21 09:54:30 +0100

    best practices on user resgistration

--- controller/auth.controllers.ts
+++ controller/auth.controllers.ts
@@ -4,26 +4,30 @@ import Logger from "../common/Logger";
 import AuthService from "../service/auth.services";
 
 export default class AuthController {
-  private prisma: PrismaClient;
-  private logger = Logger.getInstance();
-  private authService: AuthService;
+  private readonly prisma: PrismaClient;
+  private readonly logger = Logger.getInstance();
+  private readonly authService: AuthService;
 
   constructor(prisma: PrismaClient) {
     this.prisma = prisma;
     this.authService = new AuthService(prisma);
   }
 
+  /**
+   * Handles user registration.
+   * @param req - The HTTP request.
+   * @param res - The HTTP response.
+   */
   public async register(req: Request, res: Response): Promise<void> {
     try {
       // extract user data from request body
       const userData: Prisma.UserCreateInput = req.body;
+      // validate user input
+      if (!userData.email || !userData.password) {
+        throw new Error("Email and password are required.");
+      }
       // Call the registerUser method from AuthService
       const user = await this.authService.registerUser(userData);
-
-      if (!user) {
-        res.status(400).json({ message: "User registration failed" });
-        return;
-      }
       // Send a success response
       res.status(201).json({
         message: "User registered successfully",
@@ -32,9 +36,10 @@ export default class AuthController {
     } catch (error) {
       this.logger.error(`Error in register method: ${error}`);
       // Respond with an error message
-      res.status(500).json({
-        message: "Failed to register user",
-        error: error instanceof Error ? error.message : "Unknown error",
+      // Send an error response with appropriate status and message
+      res.status(400).json({
+        message:
+          error instanceof Error ? error.message : "Unknown error occurred",
       });
     }
   }
--- next.md
+++ next.md
@@ -1,3 +1,8 @@
 # implement JWT :
 # add token model and figure out how to link it with user model
 # implement complete login flow 
+
+
+
+DONT FORGOT TO HASH THE PASSWORD (SALT !!)
+
