d2ac4b6df98ccbba22abd48114d8ed0a8f582352
alouilouai <louaialoui1993@gmail.com>
2024-12-21 19:51:26 +0100

    running migration for token model + implementing the token service class

---
+++ service/token.services.ts
@@ -0,0 +1,32 @@
+import { randomBytes } from "crypto";
+import { service } from "../common/decorators/layer.decorators";
+import type { PrismaClient } from "@prisma/client";
+
+@service()
+export default class TokenService {
+  private readonly prisma: PrismaClient;
+
+  constructor(prisma: PrismaClient) {
+    this.prisma = prisma;
+  }
+  private async generateToken(
+    userId: number,
+    type: "access" | "refresh" | "validation",
+    expiresIn: number
+  ): Promise<string> {
+    const token = randomBytes(32).toString("hex");
+    const expiresAt = new Date(Date.now() + expiresIn * 1000);
+
+    //Store the token in the database
+    await this.prisma.token.create({
+      data: {
+        userId,
+        token,
+        type,
+        expiresAt,
+      },
+    });
+
+    return token;
+  }
+}
