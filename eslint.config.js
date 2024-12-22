acab7e31374eb7c8b4f92509bf32814f31133c26
alouilouai <louaialoui1993@gmail.com>
2024-12-22 16:05:15 +0100

    fixing eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,18 +2,24 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    files: ['**/*.ts', '**/*.js'],
-    extends: ['plugin:prettier/recommended'], // Add Prettier's recommended config
-    plugins: ['prettier'],
-    rules: {
-      'prettier/prettier': 'error', // Enable Prettier errors as ESLint errors
+/** @type {import('eslint').Linter.Config} */
+export default {
+  overrides: [
+    {
+      files: ['**/*.{js,mjs,cjs,ts}'],
+      languageOptions: {
+        globals: globals.browser, // Define browser globals
+      },
+      ...pluginJs.configs.recommended, // Apply eslint-plugin-js recommended config
+      ...tseslint.configs.recommended, // Apply typescript-eslint recommended config
     },
-  },
-];
+    {
+      files: ['**/*.ts', '**/*.js'],
+      extends: ['plugin:prettier/recommended'], // Add Prettier's recommended config
+      plugins: ['prettier'],
+      rules: {
+        'prettier/prettier': 'error', // Enable Prettier errors as ESLint errors
+      },
+    },
+  ],
+};
