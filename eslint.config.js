9aa55c2e52e6bea5a9804edbec577e88922aa345
alouilouai <louaialoui1993@gmail.com>
2024-12-22 16:14:10 +0100

    fixing the linting without overriding ! the new eslint uses flattened objects config

--- eslint.config.js
+++ eslint.config.js
@@ -2,25 +2,30 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-/** @type {import('eslint').Linter.Config} */
-export default {
-  overrides: [
-    {
-      files: ['**/*.{js,mjs,cjs,ts}'],
-      languageOptions: {
-        globals: globals.browser, // Define browser globals
-      },
-      ...pluginJs.configs.recommended, // Apply eslint-plugin-js recommended config
-      ...tseslint.configs.recommended, // Apply typescript-eslint recommended config
+/** @type {import('eslint').Linter.FlatConfig} */
+export default [
+  // JavaScript and TypeScript files
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: {
+      globals: globals.browser, // Define browser globals
     },
-    {
-      files: ['**/*.ts', '**/*.js'],
-      extends: ['plugin:prettier/recommended'], // Add Prettier's recommended config
-      plugins: ['prettier'],
-      rules: {
-        'prettier/prettier': 'error', // Enable Prettier errors as ESLint errors
-      },
+    plugins: ['prettier'], // Add Prettier plugin
+    ...pluginJs.configs.recommended, // Apply eslint-plugin-js recommended config
+    ...tseslint.configs.recommended, // Apply typescript-eslint recommended config
+    extends: ['plugin:prettier/recommended'], // Add Prettier's recommended config
+    rules: {
+      'prettier/prettier': 'error', // Enable Prettier errors as ESLint errors
     },
-  ],
-  ignorePatterns: ['node_modules/', 'build/', 'dist/'],
-};
+  },
+  // Additional overrides for TypeScript and JavaScript files
+  {
+    files: ['**/*.ts', '**/*.js'],
+    extends: ['plugin:prettier/recommended'], // Add Prettier's recommended config
+    plugins: ['prettier'],
+    rules: {
+      'prettier/prettier': 'error', // Enable Prettier errors as ESLint errors
+    },
+    ignorePatterns: ['node_modules/', 'build/', 'dist/'],
+  },
+];
