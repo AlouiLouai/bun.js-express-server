0f9804d78441561315ce41a4e9e01c3d62bf2e7d
alouilouai <louaialoui1993@gmail.com>
2024-12-22 16:22:31 +0100

    fixi

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,4 @@
-import js from '@eslint/js';
-
 export default [
-  // Applying the ESLint plugin for JS files
-  js.configs.recommended,
-
   // Optionally, you can add a custom config for tests if you need special rules for test files
   {
     files: ['**/*.test.js'], // Apply this configuration only to test files
@@ -23,12 +18,14 @@ export default [
   // Custom Prettier integration and rules
   {
     files: ['**/*.ts', '**/*.js'],
-    extends: ['plugin:prettier/recommended'],
     plugins: ['prettier'],
     rules: {
       'prettier/prettier': 'error', // Enable Prettier errors as ESLint errors
     },
   },
+
+  // Adding Prettier's recommended config manually
+  prettierConfig,
 
   // Ignore specific files
   {
