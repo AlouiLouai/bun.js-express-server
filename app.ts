155f2e6719cd6d0df61fbedfe7a61fe014a18263
alouilouai <louaialoui1993@gmail.com>
2024-12-23 19:05:08 +0100

    adding cors for backend

--- app.ts
+++ app.ts
@@ -4,17 +4,21 @@ import express, {
   type NextFunction,
   type Express,
 } from 'express';
+import cors from "cors";
 import process from 'node:process';
 import prisma from './prisma/prisma';
 import Config from './common/config/Config';
 import Logger from './common/Logger';
 import AuthRouter from './router/auth.router';
+import { corsConfig } from './common/config/CorsConfig';
 const app: Express = express();
 const config = Config.getInstance();
 const logger = Logger.getInstance();
 
-// Middleware to handle JSON requests
+// Middleware
+app.use(cors(corsConfig)); // Enable CORS
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded requests
 
 // Initialize and use the AuthRouter
 const authRouter = new AuthRouter(prisma);
--- next.md
+++ next.md
@@ -1,11 +1,2 @@
 ## figure out email template with express
 
-## Linting
-
-## Husky
-
-## Prettier
-
-## CI/CD
-
-## Swagger for API docs
